a91d12b0e684810de0600f37f9f869dd9cc34e02
Michael Herman <hermanmu@gmail.com>
2016-11-11 10:37:15 -0700

    updates

--- src/index.js
+++ src/index.js
@@ -5,36 +5,44 @@ import axios from 'axios';
 class FetchDemo extends React.Component {
   constructor(props) {
     super(props);
-
+    // set initial state of `posts`
     this.state = {
       posts: []
     };
   }
 
-  componentDidMount() {
+  getData() {
     axios.get(`http://www.reddit.com/r/${this.props.subreddit}.json`)
-      .then(res => {
-        const posts = res.data.data.children.map(obj => obj.data);
-        this.setState({ posts });
-      });
+    .then((res) => {
+      const posts = res.data.data.children.map((obj) => obj.data);
+      // update state with `res` from the AJAX call
+      this.setState({ posts });
+    });
   }
 
+  componentDidMount() {
+    this.getData()
+  }
+
+  // render
   render() {
     return (
       <div>
         <h1>{`/r/${this.props.subreddit}`}</h1>
         <ul>
-          {this.state.posts.map(post =>
-            <li key={post.id}>{post.title}</li>
-          )}
+          {
+            this.state.posts.map((post) =>
+            <li key={post.id}>{post.title}</li>)
+          }
         </ul>
       </div>
     );
   }
-  
+
 }
 
+// mount component to the dom
 ReactDOM.render(
-  <FetchDemo subreddit="reactjs"/>,
+  <FetchDemo subreddit="python"/>,
   document.getElementById('root')
 );
